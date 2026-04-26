e5a79349770a3476ca24e3ee1a9cfe61485f7626
Simon Vargas <sjvargas01@gmail.com>
2026-04-25 22:11:00 -0400

    binary search example

---
+++ BinarySearch.java
@@ -0,0 +1,12 @@
+public class BinarySearch {
+
+    public static int binSearch (int [] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        
+        return -1;
+
+    }
+
+}
