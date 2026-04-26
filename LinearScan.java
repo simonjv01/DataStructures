db74a97556b74cedda8ab2587bf0f0328eb32992
Simon Vargas <sjvargas01@gmail.com>
2026-04-25 21:20:11 -0400

    added example for linear search

--- LinearScan.java
+++ LinearScan.java
@@ -9,5 +9,17 @@ public class LinearScan {
         }
         return -1; // Target not found
     }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 1, 2};
+        int target = 0;
+        int result = linearSearch(arr, target);
+        
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
     
 }
