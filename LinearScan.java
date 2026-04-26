80c2bc77842b2d38c96aee621f5c7d8632da69d9
Simon Vargas <sjvargas01@gmail.com>
2026-04-25 21:17:31 -0400

    first commit

---
+++ LinearScan.java
@@ -0,0 +1,13 @@
+public class LinearScan {
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+            
+        }
+        return -1; // Target not found
+    }
+    
+}
