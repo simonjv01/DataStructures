87aead727682ef438a4e575df5b96067d3c8ee28
Simon Vargas <sjvargas01@gmail.com>
2026-04-26 10:11:13 -0400

    finished bin search example

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,9 +1,19 @@
 public class BinarySearch {
 
     public static int binSearch (int [] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
+        int low = 0;
+        int high = arr.length - 1;
 
+        while (low <= high) {
+            int indexMid = low + (high - low) / 2;
+            if (arr[indexMid] == target) {
+                return indexMid;
+            } else if (arr[indexMid] < target) {
+                low = indexMid + 1;
+            } else {
+                high = indexMid - 1;        
+            }
+            } 
         
         return -1;
 
