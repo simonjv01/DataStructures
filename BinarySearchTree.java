e591769cfe86e0914fe08aa7dca752df8c54c5cb
Simon Vargas <sjvargas01@gmail.com>
2026-04-28 21:51:17 -0400

    started binary search tree data structure example

---
+++ BinarySearchTree.java
@@ -0,0 +1,19 @@
+
+
+public class BinarySearchTree {
+
+    // Node class to represent each tree element
+    private static class Node {
+        int key;
+        Node left, right;
+
+        Node(int key) {
+            this.key = key;
+            this.left = this.right = null;
+        }
+    }
+
+    private Node root;
+
+    
+}
